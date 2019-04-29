9203ba31b4292d28f9d26e544f6c1195c1c165bc
Justin Lakkis <jlakkis@levy-ve501-0541.apn.wlan.upenn.edu>
2019-04-29 17:14:11 -0400

    summarized experiments

--- R/docluster.R
+++ R/docluster.R
@@ -10,7 +10,7 @@ docluster=function(mydata,type="uncorrected",silscores=TRUE,cosnorm=TRUE) {
         all.dists <- as.matrix(dist(t(raw.all)))
     } else if (type =='mnn') {
         Xmnn <- batchelor::mnnCorrect(raw.all, batch = batch.id, k=20, sigma=1, cos.norm.in=cosnorm, cos.norm.out=cosnorm, var.adj=TRUE)
-        Xmnn = batchelor::assays(Xmnn)
+        Xmnn = SummarizedExperiment::assays(Xmnn)
         Xmnn = Xmnn$'corrected'
         all.dists <- as.matrix(dist(t(Xmnn)))
     } else if (type =='limma') {
